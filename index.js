99a46fba314daf86a408de17c9f0f887ac93139c
ekeng <82917310+ekeng4488@users.noreply.github.com>
2022-06-17 15:45:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ import { StrictMode } from "react";
     
     import WebFont from "webfontloader";
     import App from "./App";
-
     WebFont.load({
         google: {
             families: ['Poppins', "Montserrat:300,700,500,600,400","undefined:"]
         }
       });
-
     const rootElement = document.getElementById("root");
     ReactDOM.render(
       <StrictMode>
